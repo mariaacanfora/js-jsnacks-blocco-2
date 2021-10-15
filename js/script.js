60a2ba67bd6bbc918050e980b6580a2fc030eec3
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-15 16:43:02 +0200

    Deleted alerts

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ let number = prompt("Inserisci un numero:");
 console.log("numero inserito: " + number);
 
 if (number % 2 === 0){
-    alert(number);
+    console.log("il numero è pari:" + number);
 }else{
     number++
-    console.log("numero incrementato: " + number);
-    alert((number));
+    console.log("il numero inserito è dispari. Numero incrementato: " + number);
 }
