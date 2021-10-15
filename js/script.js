ffb7bb1d6eb9c6690897f49b3bde6b762efa320a
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-15 16:39:56 +0200

    Task completed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let number = prompt("Inserisci un numero:");
+
+console.log("numero inserito: " + number);
+
+if (number % 2 === 0){
+    alert(number);
+}else{
+    number++
+    console.log("numero incrementato: " + number);
+    alert((number));
+}
